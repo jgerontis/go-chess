ae6652cee3d002ab0d6425590aef3bf326e74edc
Josh Gerontis <gerontisj@yahoo.com>
2025-03-07 14:55:28 -0800

    fix test

--- internal/chess/bitboard_test.go
+++ internal/chess/bitboard_test.go
@@ -27,30 +27,30 @@ func TestBitboardSet(t *testing.T) {
 
 func TestBitboardShift(t *testing.T) {
 	bb := Bitboard(0x8000000000000000)
-	if bb.SLeft(1) != 0 {
+	if bb<<1 != 0 {
 		t.Errorf("Expected shift left to be 0")
 	}
-	if bb.SRight(1) != 0x4000000000000000 {
+	if bb>>1 != 0x4000000000000000 {
 		t.Errorf("Expected shift right to be 0x4000000000000000")
 	}
 	newBB := NewBitboard()
 	newBB.Set(63)
-	if newBB.SLeft(1) != 0 {
+	if *newBB<<1 != 0 {
 		t.Errorf("Expected shift left to be 0")
 	}
-	if newBB.SRight(1) != 0x4000000000000000 {
+	if *newBB>>1 != 0x4000000000000000 {
 		t.Errorf("Expected shift right to be 0x4000000000000000")
 	}
 }
 
 func TestBitboardNot(t *testing.T) {
 	bb := Bitboard(0x8000000000000000)
-	if bb.Not() != 0x7FFFFFFFFFFFFFFF {
+	if ^bb != 0x7FFFFFFFFFFFFFFF {
 		t.Errorf("Expected not to be 0x7FFFFFFFFFFFFFFF")
 	}
 	newBB := NewBitboard()
 	newBB.Set(0)
-	if newBB.Not() != 0xFFFFFFFFFFFFFFFE {
+	if ^*newBB != 0xFFFFFFFFFFFFFFFE {
 		t.Errorf("Expected not to be 0xFFFFFFFFFFFFFFFE")
 	}
 }
